6297c707426fe61276d3244ec513324ba0e1fa40
Lee Carter <wleecarter@gmail.com>
2021-06-04 22:34:54 -0400

    Morgan request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
+import { MorganInterceptor, MorganModule } from 'nest-morgan';
+
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommonModule } from './common/common.module';
 
 const isProduction = process.env.NODE_ENV === 'production' ? true : false;
 
 @Module({
   imports: [
     CoffeesModule,
+    CommonModule,
     ConfigModule.forRoot({
       // set to true when deploying to providers like heroku
       ignoreEnvFile: false,
     }),
+    MorganModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST || 'localhost',
@@ -25,9 +30,14 @@ const isProduction = process.env.NODE_ENV === 'production' ? true : false;
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: !isProduction, // entities will be synced with the database (disable in prod)
     }),
-    CommonModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('dev'),
+    },
+  ],
 })
 export class AppModule {}
